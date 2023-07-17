72cf5919e80176caa25349ca2e7aaee390d2deac
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-07-17 14:06:44 -0300

    fetching cards

--- react-course/src/Pages/Home/index.jsx
+++ react-course/src/Pages/Home/index.jsx
@@ -1,11 +1,26 @@
+import { useState,useEffect } from "react";
 import Card from "../../Components/Card";
 import Layout from "../../Components/Layout";
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then(res => res.json())
+    .then(data => setItems(data))
+  },[])
+  
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      {
+        items?.map((e) => (
+          <Card key={e.id} data={e}/>
+        ))
+      }
+      </div>
     </Layout>
   );
 }
