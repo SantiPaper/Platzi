58e8b8b2ba8b7fea7ced1758a3f71e9d41c7a6e0
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-07-17 14:51:07 -0300

    create context

--- react-course/src/Components/Card/index.jsx
+++ react-course/src/Components/Card/index.jsx
@@ -1,7 +1,7 @@
  const Card = (data) => {
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
-      <figure className="relative mb-2 w-full h-4/5">
+    <div className="bg-white cursor-pointer w-56 h-605 rounded-lg">
+      <figure className="relative mb-2 w-full h-3/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {data.data.category.name}
         </span>
