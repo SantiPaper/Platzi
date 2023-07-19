a787e6f3dcc86127508962745036912209d4b41a
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-07-19 11:00:34 -0300

    Order

--- react-course/src/Components/CheckoutSideMenu/index.jsx
+++ react-course/src/Components/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import {Link} from "react-router-dom"
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
 import OrderCard from "../OrderCard";
@@ -24,7 +25,6 @@ const CheckoutSideMenu = () => {
     }
     context.setOrder([...context.order, orderToAdd])
     context.setCartProducts([])
-    console.log(context.order);
   }
 
   return (
@@ -59,7 +59,9 @@ const CheckoutSideMenu = () => {
             ${totalPrice(context.cartProducts)}
           </span>
         </p>
+        <Link to={"/my-orders-last"}>
         <button className="w-full bg-black py-3 text-white rounded-lg" onClick={() => handleCheckout()}>Checkout</button>
+        </Link>
       </div>
     </aside>
   );
--- react-course/src/Pages/MyOrder/index.jsx
+++ react-course/src/Pages/MyOrder/index.jsx
@@ -1,8 +1,26 @@
 import Layout from "../../Components/Layout"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
+import OrderCard from "../../Components/OrderCard"
+
+
 function MyOrder() {
+
+  const context = useContext(ShoppingCartContext)
     return (
       <Layout>
         MyOrder
+        <div className="px-6 flex-1">
+      {context.order?.slice(-1)[0].products.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+          ))}
+          </div>
       </Layout>
     )
   }
